7f2de1d957fe00574cdde1a3b14b5a7437fb1cc9
Yann-Eric <yann-eric@live.fr>
2014-09-30 13:34:20 +0200

    Initialissation du projet

---
+++ src/Megalo/RedirectBundle/Listener/redirectListener.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Megalo\RedirectBundle\Listener;
+
+use \Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Redirige les utilisateurs selon la liste des redirections
+ */
+class redirectListener extends Controller
+{
+
+  /**
+   * fonction de redirection point d'entree (ecoute le kernel)
+   */
+  public function onKernelSolicitation()
+  {
+    $this->actRedirects();
+  }
+
+  /**
+   * function de la redirection
+   */
+  private function actRedirects()
+  {
+    
+    $adress = 'http://'.$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    $tabRedirect = $this->getRedirects();
+
+    if (array_key_exists($adress, $tabRedirect)) {
+      header('Status: 301 Moved Permanently', false, 301);
+      header('Location: ' . $tabRedirect[$adress]);
+      exit;
+    }
+
+  }
+
+  /**
+   * Tableaux des sources
+   * @return array Tableau des redirections
+   */
+  private function getRedirects()
+  {
+    $tabRedirect = array();
+
+    $tabRedirect['http://localhost:8888/acme/web/app_dev.php/demo/hello/World'] = "http://www.google.fr";
+    $tabRedirect['http://localhost:8888/acme/web/app_dev.php/demo/secured/login'] = "http://www.yahoo.fr";
+
+    return $tabRedirect;
+  }
+
+}
---
+++ src/Megalo/RedirectBundle/MegaloRedirectBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Megalo\RedirectBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MegaloRedirectBundle extends Bundle
+{
+}
